b1ba8f8b0f6648f9a123af1edaad46a60dc75e0b
Mostafa Mamdouh <mmabdallah801@gmail.com>
2025-06-05 14:50:12 +0200

    ..

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './components/login/login.component';
 import { StudentExamsComponent } from './components/student-exams/student-exams.component';
 import { ExamDetailsComponent } from './components/student-exams/exam-details/exam-details.component';
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
+import { ResultsComponent } from './components/results/results.component';
 
 export const routes: Routes = [
   {path : "" , component : LoginComponent},
@@ -11,5 +12,6 @@ export const routes: Routes = [
   {path : "register", component : RegisterComponent},
   {path : "exams" , component : StudentExamsComponent},
   {path : "exams/:id" , component : ExamDetailsComponent},
-  {path : "dashboard" , component : AdminDashboardComponent}
+  {path : "dashboard" , component : AdminDashboardComponent},
+  {path : "results" , component : ResultsComponent}
 ];
